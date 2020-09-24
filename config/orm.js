74f5528bae89f1bd26eea334566a075996c24657
Chris Bryant <chris_bryant@mac.com>
2020-09-23 20:52:27 -0700

    Added base files

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+const connection = require("./connection");
+
+
+
+
+
+//selectAll()
+
+//insertOne()
+
+//updateOne()
+
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES
+("Cheese Burger", FALSE, CURRENT_TIMESTAMP),
+("Vegan Burger", FALSE, CURRENT_TIMESTAMP),
+("Turkey Burger", FALSE, CURRENT_TIMESTAMP);
