fc226744325a7965bebef1f489af289e2fec49ca
Chris Bryant <67081049+csbryant@users.noreply.github.com>
2020-09-30 17:39:50 -0700

    Update seeds.sql
    removed comma

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,4 +4,4 @@ INSERT INTO burgers (burger_name, devoured, date)
 VALUES
 ("Cheese Burger", FALSE, CURRENT_TIMESTAMP),
 ("Vegan Burger", FALSE, CURRENT_TIMESTAMP),
-("Turkey Burger", FALSE, CURRENT_TIMESTAMP),
+("Turkey Burger", FALSE, CURRENT_TIMESTAMP)
